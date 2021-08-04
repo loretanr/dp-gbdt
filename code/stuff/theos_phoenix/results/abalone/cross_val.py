04e3991cdabdc231863e7a75af28d7265455fe00
loretanr <loretanr@student.ethz.ch>
2021-08-04 15:43:51 +0200

    python plots done (phoenix_alltrees), RMSE looks okay, can start with C++

---
+++ code/stuff/theos_phoenix/results/abalone/cross_val.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# gtheo@ethz.ch
+"""Evaluation of our DPGBDT implementation."""
+
+import json
+import numpy as np
+from datetime import datetime
+
+from sklearn import metrics, model_selection
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import MinMaxScaler
+
+from data.parser.parser import Parser
+from evaluation import estimator
+
+# The dataset to use for evaluation
+DATASET = 'abalone'
+# The privacy budget to use for evaluation
+# PRIVACY_BUDGETS = [0.1]
+PRIVACY_BUDGETS = [0.1, 0.5, 1, 3, 8]
+# PRIVACY_BUDGETS = [0.1, 0.3, 0.6, 1, 1.5, 2, 3, 5, 7, 9]
+# The number of time to repeat the experiment to get an average accuracy
+NB_SPLITS = 5
+# Number of rows to use from the dataset
+SAMPLES = [4177]                                        # !!!!!!!!!!!!!!!!!!!!!!!
+# Nb trees for each ensemble
+NB_TREES = 50
+
+PATH = "./results/abalone/"
+
+# The optimal parameters found through the grid search for the baseline model
+with open(PATH + 'model_params.json') as json_file:
+  MODEL_PARAMS = json.load(json_file)
+
+if __name__ == '__main__':
+  now = datetime.now().strftime("%d-%m-%y_%H:%M")
+  output = open(PATH + 'results_PHOENIX_' + now + '.csv', 'a')
+  output.write('dataset,nb_samples,privacy_budget,nb_tree,nb_tree_per_ensemble,'
+               'max_depth,max_leaves,learning_rate,nb_of_runs,mean,std,'
+               'model,config,balance_partition\n')
+
+  for nb_samples in SAMPLES:
+    # Read the data
+    parser = Parser(dataset=DATASET)
+    X, y, cat_idx, num_idx, task = parser.Parse(n_rows=nb_samples)
+
+    # Own model
+    models = [estimator.DPGBDT, estimator.DPRef]
+
+    rmse = make_scorer(
+        metrics.mean_squared_error, squared=False)
+
+    for model in models:
+      model_name = str(model).split('.')[-1][:-2]
+      print('------------ Processing model: {0:s}'.format(model_name))
+      for config in ['Vanilla', 'DFS']:
+        for idx, budget in enumerate(PRIVACY_BUDGETS):
+          if config == 'Vanilla' and idx != 0:
+            continue
+          if model_name == 'DPRef' and config != 'DFS':
+            continue
+          model_params = MODEL_PARAMS.get(model_name).get(config)
+          if config == 'Vanilla':
+            budget = 0.
+          m = model(  # type: ignore
+              budget,
+              NB_TREES,
+              NB_TREES,
+              model_params.get('max_depth'),
+              model_params.get('learning_rate'),
+              n_classes=len(set(y)) if task == 'classification' else None,
+              gradient_filtering=False,
+              leaf_clipping=model_params.get('leaf_clipping'),
+              max_leaves=model_params.get('max_leaves'),
+              min_samples_split=2,
+              balance_partition=True,
+              use_bfs=False,
+              use_3_trees=False,
+              cat_idx=cat_idx,
+              num_idx=num_idx,
+              verbosity=0)  # type: ignore
+          regressor = TransformedTargetRegressor(regressor=m)     # REMOVED MINMAXSCALER
+          validator = model_selection.KFold(n_splits=NB_SPLITS, random_state=np.random.randint(0,100000) ,shuffle=True)               # somehow shuffle=True did not shuffle!!!!
+          # validator = model_selection.KFold(n_splits=NB_SPLITS, shuffle=False)               # somehow shuffle=True did not shuffle!!!!
+          scores = cross_val_score(                                           # leaving it away now shuffles!
+              regressor, X, y, cv=validator, scoring=rmse, n_jobs=-1)
+          mean, std = scores.mean(), (scores.std() / 2)
+          output.write(
+              '{0:s},{1:d},{2:f},{3:d},{4:d},{5:d},{6:d},{7:f},'  # type: ignore
+              '{8:d},{9:f},{10:f},{11:s},{12:s},{13:s}\n'.format(
+                  DATASET, min(nb_samples, len(y)), budget, NB_TREES,
+                  NB_TREES,
+                  model_params.get('max_depth'),
+                  -1, model_params.get('learning_rate'), NB_SPLITS, mean, std,
+                  model_name, config,
+                  str(model_params.get('balance_partition'))))
+  output.close()
---
+++ code/stuff/theos_phoenix/results/abalone/plot.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# gtheo@ethz.ch
+"""Plot results."""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from evaluation import estimator
+
+PATH = './results/abalone/'
+NAME = 'results_AFTERMORITZ_04-08-21_09:07.csv'
+SAMPLES = [4177]
+
+if __name__ == '__main__':
+  data = pd.read_csv(PATH + NAME, usecols=[
+      'dataset', 'nb_samples', 'privacy_budget', 'nb_tree',
+      'nb_tree_per_ensemble', 'max_depth',
+      'max_leaves', 'learning_rate', 'nb_of_runs', 'mean', 'std', 'model',
+      'config', 'balance_partition'])
+
+  # privacy_budgets = [0.1, 0.3, 0.6, 1, 1.5, 2, 3, 5, 7, 9]
+  privacy_budgets = [0.1]
+  param_values = data.iloc[0]
+
+  # Own model
+  models = [estimator.DPGBDT, estimator.DPRef]
+
+  for nb_samples in SAMPLES:
+    plt.clf()
+    plt.grid(True)
+    for model in models:
+      model_name = str(model).split('.')[-1][:-2]
+      for config in ['DFS','Vanilla']:
+        if config in ['BFS']:
+          continue
+        if model_name == 'DPRef' and config != 'DFS':
+          continue
+        values = data[(data['model'] == model_name) & (
+            data['config'] == config) & (data['nb_samples'] == nb_samples)]
+        if config != 'Vanilla':
+          mean = values['mean']
+          std = values['std']
+          if model_name == 'DPRef':
+            marker = '-v'
+            label = 'DPRef'
+          else:
+            marker = '-^'
+            label = 'DPGBDT (DFS)'
+        else:
+          mean = list([values['mean'].values[0] for _ in range(
+            len(privacy_budgets))])
+          std = list([values['std'].values[0] for _ in range(
+            len(privacy_budgets))])
+          label = 'GBDT (Vanilla)'
+          marker = '-ko'
+
+        plt.errorbar(privacy_budgets,
+                     mean,
+                     yerr=std,
+                     fmt=marker,
+                     capsize=3,
+                     label=label)
+
+    plt.axis([0, int(max(privacy_budgets)), 0, int(max(
+        data[data['nb_samples'] == nb_samples]['mean']) + max(
+            data[data['nb_samples'] == nb_samples]['std']) ) * 1.1 ])
+    plt.legend(loc='upper right')
+    plt.title('Dataset={0!s}, Samples={1!s}, Trees={2!s}'.format(
+        param_values['dataset'], nb_samples,
+        data[data['nb_samples'] == nb_samples].iloc[0]['nb_tree']))
+    plt.xlabel('Privacy budget')
+    plt.ylabel('RMSE')
+    plt.savefig(
+        PATH + NAME.rsplit('.',1)[0]  + '.png', format='png', dpi=600)
