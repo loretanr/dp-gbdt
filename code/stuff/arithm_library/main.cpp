a04515378fbb9e0791893fd78a18ec2fab2f3ee2
loretanr <loretanr@student.ethz.ch>
2021-09-24 17:42:06 +0200

    start with floatlib experiments

---
+++ code/stuff/arithm_library/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned a = 1, b = 2, c;
+
+    c = a + b;
+
+    cout << c << endl;
+
+    c = a * b;
+
+    cout << c << endl;
+
+    return c;
+
+    // TODO do the floatmath import here
+}
